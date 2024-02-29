eaa87f61976547914eff137b94a6043dacb7d0be
camila <camila.ana.araujo@escola.pr.gov.br>
2024-02-29 09:37:52 -0300

    first commt

---
+++ saiaedados.py
@@ -0,0 +1,19 @@
+#adição
+print(1+1) 
+
+#subtração
+print(10-2)
+
+#multiplicação
+print(10*3)
+
+#divisão 
+print(12/4)
+
+#exponenciação
+prit(5**2)
+
+#modulo
+print(10%2)
+
+PRINT("====calculos aritiméticos com variáveis====")
